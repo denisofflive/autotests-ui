2d90842f681dc6a7be13f56aa5aeb0cca374ba14
denisofflive <denisofflive@mail.ru>
2026-05-29 00:11:27 +0300

    Практика работы с pytest фикстурами

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,57 +1,22 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import expect, Page
+
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list():
-    # Открываем браузер с использованием Playwright
-    with sync_playwright() as playwright:
-        # Запускаем Chromium браузер в обычном режиме (не headless)
-        browser = playwright.chromium.launch(headless=False)
-        # Создаем новый контекст браузера (новая сессия, которая изолирована от других)
-        context = browser.new_context()
-        # Открываем новую страницу в рамках контекста
-        page = context.new_page()
-
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('user.name@gmail.com')
-
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
-
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('password')
-
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state="browser-state.json")  # Указываем файл с сохраненным состоянием
-        page = context.new_page()
+def test_empty_courses_list(chromium_page_with_state):
+        page = chromium_page_with_state
 
         page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-        # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
-        context.storage_state(path="browser-state.json")
+        toolbar_title = page.get_by_test_id("courses-list-toolbar-title-text")
+        expect(toolbar_title).to_have_text("Courses")
 
-        # Проверяем, что отображается заголовок "Courses"
-        courses = page.get_by_test_id('courses-list-toolbar-title-text')
-        expect(courses).to_be_visible()
-        expect(courses).to_have_text("Courses")
+        no_results_text = page.get_by_test_id("courses-list-empty-view-title-text")
+        expect(no_results_text).to_have_text("There is no results")
 
-        # Проверяем, что отображается Иконка
-        icon = page.get_by_test_id('courses-list-empty-view-icon')
-        expect(icon).to_be_visible()
+        empty_list_icon = page.get_by_test_id("courses-list-empty-view-icon")
+        expect(empty_list_icon).to_be_visible()
 
-        # Проверяем, что отображается блок "There is no results"
-        block_results = page.get_by_test_id('courses-list-empty-view-title-text')
-        expect(block_results).to_be_visible()
-        expect(block_results).to_have_text("There is no results")
-
-        # Проверяем, что отображается текст "Results from the load test pipeline will be displayed here"
-        text_results = page.get_by_test_id('courses-list-empty-view-description-text')
-        expect(text_results).to_be_visible()
-        expect(text_results).to_have_text("Results from the load test pipeline will be displayed here")
+        empty_description = page.get_by_test_id("courses-list-empty-view-description-text")
+        expect(empty_description).to_have_text("Results from the load test pipeline will be displayed here")
