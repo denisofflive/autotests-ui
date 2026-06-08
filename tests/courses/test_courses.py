ea48a19da03bb952937242e61f21485b7a487a97
denisofflive <denisofflive@mail.ru>
2026-06-08 23:16:02 +0300

    Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 
 from pages.courses.courses_list_page import CoursesListPage
@@ -39,4 +41,39 @@ class TestCourses:
         courses_list_page.toolbar_view.check_visible()
         courses_list_page.course_view.check_visible(
             index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
+        )
+
+    def test_edit_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+        create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+
+        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+        create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+        create_course_page.create_course_form.fill(
+            title='Playwright',
+            estimated_time='2 weeks',
+            description='Playwright',
+            max_score='100',
+            min_score='10'
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.check_current_url(re.compile(".*/#/courses"))
+        courses_list_page.course_view.image.check_visible()
+        courses_list_page.course_view.menu.click_edit(0)
+
+        create_course_page.create_course_form.fill(
+            title='Changed title',
+            estimated_time='Changed estimated time',
+            description='Changed description',
+            max_score='1000',
+            min_score='1'
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title='Changed title',
+            estimated_time='Changed estimated time',
+            max_score='1000',
+            min_score='1'
         )
