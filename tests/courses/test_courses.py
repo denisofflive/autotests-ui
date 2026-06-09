c8cca7c74bf0cb89f1c2e82e63135fce1ccddab6
denisofflive <denisofflive@mail.ru>
2026-06-09 21:43:50 +0300

    Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,5 +1,3 @@
-import re
-
 import pytest
 
 from pages.courses.courses_list_page import CoursesListPage
@@ -11,8 +9,10 @@ from pages.courses.create_course_page import CreateCoursePage
 class TestCourses:
     def test_empty_courses_list(self, courses_list_page: CoursesListPage):
         courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-        courses_list_page.navbar.check_visible('username')
+
+        courses_list_page.navbar.check_visible("username")
         courses_list_page.sidebar.check_visible()
+
         courses_list_page.toolbar_view.check_visible()
         courses_list_page.check_visible_empty_view()
 
@@ -27,7 +27,7 @@ class TestCourses:
         create_course_page.create_course_exercises_toolbar_view.check_visible()
         create_course_page.check_visible_exercises_empty_view()
 
-        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+        create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
         create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
         create_course_page.create_course_form.fill(
             title="Playwright",
@@ -43,37 +43,35 @@ class TestCourses:
             index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
         )
 
-    def test_edit_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
-        create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
-
-        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+    def test_edit_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
+        create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+        create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
         create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
         create_course_page.create_course_form.fill(
-            title='Playwright',
-            estimated_time='2 weeks',
-            description='Playwright',
-            max_score='100',
-            min_score='10'
+            title="Playwright",
+            estimated_time="2 weeks",
+            description="Playwright",
+            max_score="100",
+            min_score="10"
         )
         create_course_page.create_course_toolbar_view.click_create_course_button()
 
-        courses_list_page.check_current_url(re.compile(".*/#/courses"))
-        courses_list_page.course_view.image.check_visible()
-        courses_list_page.course_view.menu.click_edit(0)
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
+            index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks"
+        )
+        courses_list_page.course_view.menu.click_edit(index=0)
 
         create_course_page.create_course_form.fill(
-            title='Changed title',
-            estimated_time='Changed estimated time',
-            description='Changed description',
-            max_score='1000',
-            min_score='1'
+            title="New Playwright",
+            estimated_time="3 weeks",
+            description="New Playwright",
+            max_score="1000",
+            min_score="100"
         )
         create_course_page.create_course_toolbar_view.click_create_course_button()
 
+        courses_list_page.toolbar_view.check_visible()
         courses_list_page.course_view.check_visible(
-            index=0,
-            title='Changed title',
-            estimated_time='Changed estimated time',
-            max_score='1000',
-            min_score='1'
+            index=0, title="New Playwright", max_score="1000", min_score="100", estimated_time="3 weeks"
         )
