8cb67e0975fac6598464552ac130cdf36b7a1204
denisofflive <denisofflive@mail.ru>
2026-06-21 23:56:24 +0300

    Добавляем информацию о переменных окружения в Allure отчет

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 pytest_plugins = (
-    "fixtures.pages", # Подключаем фикстуры страниц
-    "fixtures.browsers" # Подключали ранее в предыдущих уроках
+    "fixtures.pages",  # Подключаем фикстуры страниц
+    "fixtures.allure",
+    "fixtures.browsers"  # Подключали ранее в предыдущих уроках
 )
