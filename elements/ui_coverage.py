2e67ec814ccae0c749f7a332cb7d107ed23ed657
denisofflive <denisofflive@mail.ru>
2026-06-28 23:19:36 +0300

    Практическое задание: Работа с инструментом ui-coverage-tool

--- elements/ui_coverage.py
+++ elements/ui_coverage.py
@@ -3,3 +3,4 @@ from ui_coverage_tool import UICoverageTracker
 # Инициализируем трекер для нашего приложения "ui-course"
 # ВАЖНО: 'ui-course' должен точно совпадать с ключом `key` в UI_COVERAGE_APPS
 tracker = UICoverageTracker(app="ui-course")
+
